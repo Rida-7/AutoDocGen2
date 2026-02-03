618ec82169c0bef256eb1c23f2185988452a17a0
momina-rashad <mominarashad137@gmail.com>
2026-02-03 20:23:26 +0500

    Update db.py

--- app/db.py
+++ app/db.py
@@ -2,8 +2,8 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 import os
 
-MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017")
-DB_NAME = os.getenv("DB_NAME", "autodocgen")
+MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://hadiamoosa40_db_user:op6v4ma4mlzA75wF@users.cy4zf7c.mongodb.net/Doc_Gen?retryWrites=true&w=majority")
+DB_NAME = os.getenv("DB_NAME", "Doc_Gen")
 
 client = AsyncIOMotorClient(MONGO_URI)
 db = client[DB_NAME]
