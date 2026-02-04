4aa48a8d0e399e05213a72ad0d9ee9b5684c173b
momina-rashad <mominarashad137@gmail.com>
2026-02-04 16:30:11 +0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -34,10 +34,7 @@ app = FastAPI()
 # ------------------ CORS ------------------
 origins = [
     FRONTEND_URL,
-    "http://localhost:5173",
-    "http://localhost:5174",
-    "http://127.0.0.1:5173",
-    "http://127.0.0.1:5174",
+    "http://localhost:5173"
 ]
 
 app.add_middleware(
