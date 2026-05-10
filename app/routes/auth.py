875c4224e3437afa13feb36ce7187187b2712c4b
Rida-7 <ridabatool7450@gmail.com>
2026-05-10 12:55:57 +0500

    auth.py updated

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -194,7 +194,7 @@ async def google_callback(request: Request):
     resp = HTMLResponse(
         '<script>'
         f'localStorage.setItem("loggedInUser", {safe_user_json});'
-        f'window.location.href = "{FRONTEND_URL}/landing";'
+        f'window.location.href = "{FRONTEND_URL}/dashboard";'
         '</script>'
     )
     issue_token(resp, safe_user)
@@ -267,7 +267,7 @@ async def github_callback(request: Request):
     resp = HTMLResponse(
         '<script>'
         f'localStorage.setItem("loggedInUser", {safe_user_json});'
-        f'window.location.href = "{FRONTEND_URL}/landing";'
+        f'window.location.href = "{FRONTEND_URL}/dashboard";'
         '</script>'
     )
     issue_token(resp, safe_user)
